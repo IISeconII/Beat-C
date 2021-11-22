6db2f42255840b6d660142086645f1790d09d5d8
Secon0101 <duwnsgur11012@gmail.com>
2021-11-22 22:13:35 +0900

    키설정 UI 개선

--- keysetting.c
+++ keysetting.c
@@ -1,5 +1,4 @@
 /* keysetting.c */
-
 #include "keysetting.h"
 
 
@@ -9,8 +8,12 @@ void main_keysetting() {
 	gotoxy(1, 1);
 	puts("Å° ¼³Á¤");
 
-	gotoxy(1, 2);
 	for (int i = 0; i < LINE; i++) {
-		key[i] = _getche();
+		gotoxy(1+i*2, 3); _putch('v');
+	}
+	for (int i = 0; i < LINE; i++) {
+		gotoxy(1+i*2, 7); _putch('^');
+		gotoxy(1+i*2, 5); key[i] = _getche();
+		gotoxy(1+i*2, 7); _putch(' ');
 	}
 }
