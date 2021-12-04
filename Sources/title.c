321096feae8f4cb1daed7fb96ef4a051623badcc
Secon0101 <duwnsgur11012@gmail.com>
2021-12-05 00:46:44 +0900

    타이틀 로고 변경

--- Sources/title.c
+++ Sources/title.c
@@ -8,30 +8,21 @@ void main_title() {
 
 	system("cls");
 
-	char title[7][75] = {
-		":::::::::  ::::::::::     :::     :::::::::::                :::::::: ",
-		":+:    :+: :+:          :+: :+:       :+:                   :+:    :+:",
-		"+:+    +:+ +:+         +:+   +:+      +:+                   +:+       ",
-		"+#++:++#+  +#++:++#   +#++:++#++:     +#+     +#++:++#++:+  +#+       ",
-		"+#+    +#+ +#+        +#+     +#+     +#+                   +#+       ",
-		"#+#    #+# #+#        #+#     #+#     #+#                   #+#    #+#",
-		"#########  ########## ###     ###     ###                    ######## ",
-	};
-	const int titleWid = (int)strlen(title[0]); // 70
-	const int titleHei = sizeof(title) / sizeof(title[0]); // 7
-
 	for (int i = 0; i < titleHei; i++) {
 		gotoxy(SCREEN_WIDTH/2 - titleWid/2, 4+i);
-		puts(title[i]);
+		puts(beatc[i]);
 	}
 
+	const char title[] = "Beat-C";
+	gotoxy(SCREEN_WIDTH/2 - (int)strlen(title)/2, 4+titleHei+2);
+	puts(title);
 	const char anykey[] = "시작하려면 아무 키나 누르세요";
-	gotoxy(SCREEN_WIDTH/2 - (int)strlen(anykey)/2, 4+titleHei+2);
+	gotoxy(SCREEN_WIDTH/2 - (int)strlen(anykey)/2, 4+titleHei+4);
 	puts(anykey);
 
-	gotoxy(SCREEN_WIDTH/2, 4+titleHei+3);
+	gotoxy(SCREEN_WIDTH/2, 4+titleHei+5);
 	setCursor(UNDERBAR);
-	int _ = _getch();
+	clearBuffer();
 
-	mapPath = "maps/"; // 미리 할당
+	int _ = _getch();
 }
